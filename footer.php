6cba177fb093e39013eed1213b9bbd8ba411cb71
Frank Bültge <frank@bueltge.de>
2012-06-12 22:32:25 +0200

    change name for class folder, to see it is old, but the functions is very usefull - will integrate this in the other folder, specialy in inc
    git push

--- footer.php
+++ footer.php
@@ -1,13 +1,26 @@
+<?php
+/**
+ * The Template for displaying the footer.
+ * 
+ * @package    WP Basis
+ * @subpackage 
+ * @since      06/12/2012  0.0.1
+ * @version    06/12/2012
+ * @author     fb
+ */
+?>
 		
 		</div> <?php // end #main in header.php ?>
 		
-	</div> <?php // end #wrap in header.php ?>
-	
-		<footer>
-			<p>
-				<?php echo $wpdb->num_queries; ?>q, <?php timer_stop(1); ?>s
-			</p>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php do_action( 'wp_basis_footer_credits' );
+			printf( __( '<p class="site-info">Theme: %1$s by %2$s.</p>', 'wp_basis' ), 'WP Basis', '<a href="https://github.com/bueltge/WordPress-Basis-Theme/network/members" rel="designer">Contributer Team</a>' );
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG )
+				printf( __( '<p>Generate Page with %1$sq in %2$ss.</p>', 'wp_basis' ), $wpdb->num_queries, timer_stop( 0 ) );
+			?>
 		</footer>
+		
+	</div> <?php // end #wrap in header.php ?>
 	
 	<?php wp_footer(); ?>
 	
