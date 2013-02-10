32d2eccb6e8d2fef887dcccf0cbbddbae73a6a46
Frank Bültge <frank@bueltge.de>
2013-02-10 20:31:49 +0100

    Provide a context for the comments title strings

--- comments.php
+++ comments.php
@@ -33,7 +33,7 @@
 		<h2 id="comments-title">
 			<?php
 			printf( 
-				_n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;',get_comments_number(), 'wp_basis' ),
+				_nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'wp_basis' ),
 				number_format_i18n( get_comments_number() ),
 				'<span>' . get_the_title() . '</span>'
 			);
