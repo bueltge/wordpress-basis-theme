93c79987a48abfd37b9dad38d33335f86d09ec2c
Frank Bueltge <frank@bueltge.de>
2018-01-03 13:08:21 +0100

    Add namespace seperator

--- inc/class-gutenberg.php
+++ inc/class-gutenberg.php
@@ -29,7 +29,7 @@ function init_gutenberg() {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/reference/theme-support/
 	 */
-	add_theme_support(
+	\add_theme_support(
 		'gutenberg',
 		array(
 			// Full width images and other content such as videos.
@@ -58,12 +58,12 @@ function enqueue_style() {
 	// set suffix for debug mode
 	$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-	wp_register_style(
+	\wp_register_style(
 		'wp_basis_gutenberg',
-		get_template_directory_uri() . '/assets/css/blocks' . $suffix . '.css',
+		\get_template_directory_uri() . '/assets/css/blocks' . $suffix . '.css',
 		false,
 		'2018-01-03',
 		'screen'
 	);
-	wp_enqueue_style( 'wp_basis_gutenberg' );
+	\wp_enqueue_style( 'wp_basis_gutenberg' );
 }
