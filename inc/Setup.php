dcdd317527d59d7c383b5d005d2dbaa82d0bbc11
bueltge <frank@bueltge.de>
2018-01-04 16:57:25 +0100

    codex cleanup

--- inc/Setup.php
+++ inc/Setup.php
@@ -11,12 +11,13 @@
 
 /**
  * Set namespace to encapsulating items
+ *
  * @link       http://www.php.net/manual/en/language.namespaces.rationale.php
- * 
  * @since      2012-05-08  0.0.1
  * @version    2012-05-08
  * @author     Frank Bültge <frank@bueltge.de>
  */
+
 namespace Wp_Basis\Setup;
 
 use Wp_Basis\Admin_Branding\Wp_Basis_Admin_Branding;
@@ -24,18 +25,17 @@ use Wp_Basis\Core\Core;
 use Wp_Basis\Gutenberg\Gutenberg;
 use Wp_Basis\I18n\I18n;
 
+add_action( 'init', __NAMESPACE__ . '\\setup' );
 /**
  * Sets up theme defaults and registers support for various WordPress features.
- *
  * Note that this function is hooked into the after_setup_theme hook, which runs
  * before the init hook. The init hook is too late for some features, such as indicating
  * support post thumbnails.
- * 
+ *
  * @since      2012-05-08  0.0.1
  * @version    2014-04-29
  * @author     Frank Bültge <frank@bueltge.de>
  */
-add_action( 'init', __NAMESPACE__ . '\\setup' );
 function setup() {
 
 	/**
@@ -44,7 +44,7 @@ function setup() {
 	 *
 	 * @since    2012-05-08  0.0.1
 	 */
-	$GLOBALS['content_width'] = 640; /* pixels */
+	$GLOBALS[ 'content_width' ] = 640; /* pixels */
 
 	/**
 	 * Set the prefix for each usage.
@@ -114,20 +114,20 @@ function setup() {
  */
 require_once __DIR__ . '/../widgets/Widgets_Init.php';
 
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\scripts' );
 /**
  * Enqueue Scripts and Style.
  *
  * @ToDo we should switch this in separately class.
  */
-add_action( 'wp_enqueue_scripts', '\Wp_Basis\Setup\scripts' );
 function scripts() {
-	
+
 	// set suffix for debug mode
 	$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-	
+
 	// load stylesheet inside the css folder
 	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/assets/css/style' . $suffix . '.css' );
-	
+
 	// load comment reply script, if comments open, if thread comments are active and also on single view
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
